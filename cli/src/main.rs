525919d8d3738e1bb5e39fa156adb3c5b4faa799
Klaus Ma <klausm@nvidia.com>
2023-03-05 15:48:46 +0800

    print more info of pkey.
    Signed-off-by: Klaus Ma <klausm@nvidia.com>

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -5,7 +5,12 @@ async fn main() -> Result<(), UFMError> {
     let mut ufm = UFM::new()?;
     let p = ufm.get_partition(&"0x7fff".to_string()).await?;
 
-    println!("{} {}\n", p.name, p.pkey);
+    println!("{:15}: {}", "Name", p.name);
+    println!("{:15}: 0x{:x}", "Pkey", p.pkey);
+    println!("{:15}: {}", "IPoIB", p.ipoib);
+    println!("{:15}: {}", "MTU", p.qos.mtu_limit);
+    println!("{:15}: {}", "Rate Limit", p.qos.rate_limit);
+    println!("{:15}: {}", "Service Level", p.qos.service_level);
 
     Ok(())
 }
